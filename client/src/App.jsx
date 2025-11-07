89230674a388b0c4787aec7a01e3c51e4d206a21
ChristianMusico <chrstnmusico@gmail.com>
2025-11-07 12:22:52 +0800

    Navbar and link

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import About from './pages/About.jsx'
 import Home from './pages/Home.jsx'
+import Navbar from './components/Navbar.jsx'
+import Person from './pages/Person.jsx'
+import NotFound from './pages/NotFound.jsx'
+import { Route, Routes } from 'react-router'
 const App = () => {
   return (
     <div>
-      <Home />  
+      <Navbar />
+
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path='about' element={<About />} />
+        <Route path='person' element={<Person />} />
+        <Route path='*' element={<NotFound />} />
+      </Routes>
     </div>
   )
 }
